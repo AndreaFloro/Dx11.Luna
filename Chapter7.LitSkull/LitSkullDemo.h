769058b0c0880c9ef6225424484aa5629ac6d113
AndreaFloro <andreafloro@outlook.it>
2017-01-22 22:00:23 +0100

    Chapter 7 LitSkull demo

---
+++ Chapter7.LitSkull/LitSkullDemo.h
@@ -0,0 +1,81 @@
+#pragma once
+
+#include "d3dApp.h"
+#include "d3dx11Effect.h"
+#include "GeometryGenerator.h"
+#include "MathHelper.h"
+#include "LightHelper.h"
+#include "Effects.h"
+#include "Vertex.h"
+
+class LitSkullApp : public D3DApp
+{
+public:
+	LitSkullApp(HINSTANCE hInstance);
+	~LitSkullApp();
+
+	bool Init();
+	void OnResize();
+	void UpdateScene(float dt);
+	void DrawScene();
+
+	void OnMouseDown(WPARAM btnState, int x, int y);
+	void OnMouseUp(WPARAM btnState, int x, int y);
+	void OnMouseMove(WPARAM btnState, int x, int y);
+
+private:
+	void BuildShapeGeometryBuffers();
+	void BuildSkullGeometryBuffers();
+
+private:
+	ID3D11Buffer* mShapesVB;
+	ID3D11Buffer* mShapesIB;
+
+	ID3D11Buffer* mSkullVB;
+	ID3D11Buffer* mSkullIB;
+
+	DirectionalLight mDirLights[3];
+	Material mGridMat;
+	Material mBoxMat;
+	Material mCylinderMat;
+	Material mSphereMat;
+	Material mSkullMat;
+
+	// Define transformations from local spaces to world space.
+	DirectX::XMFLOAT4X4 mSphereWorld[10];
+	DirectX::XMFLOAT4X4 mCylWorld[10];
+	DirectX::XMFLOAT4X4 mBoxWorld;
+	DirectX::XMFLOAT4X4 mGridWorld;
+	DirectX::XMFLOAT4X4 mSkullWorld;
+
+	DirectX::XMFLOAT4X4 mView;
+	DirectX::XMFLOAT4X4 mProj;
+
+	int mBoxVertexOffset;
+	int mGridVertexOffset;
+	int mSphereVertexOffset;
+	int mCylinderVertexOffset;
+
+	UINT mBoxIndexOffset;
+	UINT mGridIndexOffset;
+	UINT mSphereIndexOffset;
+	UINT mCylinderIndexOffset;
+
+	UINT mBoxIndexCount;
+	UINT mGridIndexCount;
+	UINT mSphereIndexCount;
+	UINT mCylinderIndexCount;
+
+	UINT mSkullIndexCount;
+
+	UINT mLightCount;
+
+	DirectX::XMFLOAT3 mEyePosW;
+
+	float mTheta;
+	float mPhi;
+	float mRadius;
+
+	POINT mLastMousePos;
+};
+
