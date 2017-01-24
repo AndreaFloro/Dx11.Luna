0cfea7397dd9f7a4dec0597d0da0b4dbf7905e31
AndreaFloro <andreafloro@outlook.it>
2017-01-24 23:34:09 +0100

    Added Chapter8 (texturing) crate sample

---
+++ Chapter8.CrateDemo/CrateApp.h
@@ -0,0 +1,55 @@
+#pragma once
+#include "d3dApp.h"
+#include "d3dx11Effect.h"
+#include "GeometryGenerator.h"
+#include "MathHelper.h"
+#include "LightHelper.h"
+#include "Effects.h"
+#include "Vertex.h"
+
+class CrateApp : public D3DApp
+{
+public:
+	CrateApp(HINSTANCE hInstance);
+	~CrateApp();
+
+	bool Init();
+	void OnResize();
+	void UpdateScene(float dt);
+	void DrawScene();
+
+	void OnMouseDown(WPARAM btnState, int x, int y);
+	void OnMouseUp(WPARAM btnState, int x, int y);
+	void OnMouseMove(WPARAM btnState, int x, int y);
+
+private:
+	void BuildGeometryBuffers();
+
+private:
+	ID3D11Buffer* mBoxVB;
+	ID3D11Buffer* mBoxIB;
+
+	ID3D11ShaderResourceView* mDiffuseMapSRV;
+
+	DirectionalLight mDirLights[3];
+	Material mBoxMat;
+
+	DirectX::XMFLOAT4X4 mTexTransform;
+	DirectX::XMFLOAT4X4 mBoxWorld;
+
+	DirectX::XMFLOAT4X4 mView;
+	DirectX::XMFLOAT4X4 mProj;
+
+	int mBoxVertexOffset;
+	UINT mBoxIndexOffset;
+	UINT mBoxIndexCount;
+
+	DirectX::XMFLOAT3 mEyePosW;
+
+	float mTheta;
+	float mPhi;
+	float mRadius;
+
+	POINT mLastMousePos;
+};
+
